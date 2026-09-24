ddf0c7e9465329a1d57a0ad5f4642fbdb55b7f3f
js <js@eworx.gr>
2026-09-24 11:32:48 +0300

    Refs #308580 Dockerfile refactoring

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,48 @@
 FROM tomcat:6
+
+# 1. Create directory structures
+RUN mkdir -p ${CATALINA_HOME}/lib/org/apache/catalina/authenticator \
+    && mkdir -p ${CATALINA_HOME}/lib/org/apache/catalina/startup \
+    && mkdir -p /tmp/certs
+
+# 2. Download static configurations and JARs
+ADD https://raw.githubusercontent.com/eea/aqr-public/master/ServerConfiguration/ecas/tomcat/org/apache/catalina/authenticator/mbeans-descriptors.xml ${CATALINA_HOME}/lib/org/apache/catalina/authenticator/mbeans-descriptors.xml
+ADD https://raw.githubusercontent.com/eea/aqr-public/master/ServerConfiguration/ecas/tomcat/org/apache/catalina/startup/Authenticators.properties ${CATALINA_HOME}/lib/org/apache/catalina/startup/Authenticators.properties
+ADD https://github.com/eea/aqr-public/raw/master/ServerConfiguration/ecas/tomcat/ecas-tomcat-5.5-3.6.3.jar ${CATALINA_HOME}/lib/ecas-tomcat-5.5-3.6.3.jar
+ADD https://github.com/eea/aqr-public/raw/master/ServerConfiguration/ecas/tomcat/log4j-1.2.17.jar ${CATALINA_HOME}/lib/log4j-1.2.17.jar
+ADD https://github.com/eea/aqr-public/raw/master/ServerConfiguration/log4j/tomcat-juli-adapters.jar ${CATALINA_HOME}/lib/tomcat-juli-adapters.jar
+ADD https://github.com ${CATALINA_HOME}/lib/tomcat-juli.jar
+ADD https://github.com/eea/aqr-public/raw/master/ServerConfiguration/log4j/tomcat-juli.jar ${CATALINA_HOME}/lib/log4j.properties
+
+# 3. Download and verify certificates during the build phase
+ADD https://raw.githubusercontent.com/eea/aqr-public/master/ServerConfiguration/ecas/certs/CommisSign.cer /tmp/certs/CommisSign.cer
+ADD https://raw.githubusercontent.com/eea/aqr-public/master/ServerConfiguration/ecas/certs/EuropeanCommission.cer /tmp/certs/EuropeanCommission.cer
+ADD https://raw.githubusercontent.com/eea/aqr-public/master/ServerConfiguration/ecas/certs/GlobalSign.cer /tmp/certs/GlobalSign.cer
+
+# 4. Verify checksums and import certificates into the Java Keystore
+RUN set -e; \
+    EUROCOM_SHA="70a7c70604a20c0fedc704351637efb9ff298b4f"; \
+    COMMIS_SHA="b7e343a36e8bfbe5154250c1987f2efc6c396abb"; \
+    GLOBAL_SHA="461ad9496b06e2a8ad0df391367b16d00c8f8fc7"; \
+    if [ "$(sha1sum /tmp/certs/EuropeanCommission.cer | awk '{print $1}')" != "$EUROCOM_SHA" ] || \
+       [ "$(sha1sum /tmp/certs/CommisSign.cer | awk '{print $1}')" != "$COMMIS_SHA" ] || \
+       [ "$(sha1sum /tmp/certs/GlobalSign.cer | awk '{print $1}')" != "$GLOBAL_SHA" ]; then \
+        printf "Invalid certificate sha1sum discovered during build.\n" && exit 1; \
+    fi; \
+    keytool -import -noprompt -v -keystore cacerts -storepass changeit -alias EuropeanCommission -file /tmp/certs/EuropeanCommission.cer \
+    && keytool -import -noprompt -v -keystore cacerts -storepass changeit -alias CommisSign -file /tmp/certs/CommisSign.cer \
+    && keytool -import -noprompt -v -keystore cacerts -storepass changeit -alias GlobalSign -file /tmp/certs/GlobalSign.cer \
+    && rm -rf /tmp/certs
+
+# 5. Perform server configuration tweaks
+RUN rm -f ${CATALINA_HOME}/conf/logging.properties
+COPY server.xml ${CATALINA_HOME}/conf/server.xml
+
+# 6. Handle the application WAR file (the produced aqrsystem.war must exist in the current directory)
+ENV WAR_FILEPATH=/tmp/ROOT.war
+COPY aqrsystem.war ${WAR_FILEPATH}
+
+# 7. Final setup
 COPY docker-entrypoint.sh /entrypoint.sh
-ENV WAR_FILEPATH /tmp/ROOT.war
 RUN chmod +x /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,111 +1,18 @@
 #!/bin/bash
-#: File: docker-entrypoint.sh
-#: Description: 
-#: Imports EuropeanCommission, CommisSign and GlobalSign certificates, configures tomcat ECAS Authenticator, deploys the war.
-#: 
-
 set -e
 
-BEANS_XML_URL="https://raw.githubusercontent.com/eea/aqr-public/master/ServerConfiguration/ecas/tomcat/org/apache/catalina/authenticator/mbeans-descriptors.xml"
-
-mkdir -p ${CATALINA_HOME}/lib/org/apache/catalina/authenticator
-curl -L "$BEANS_XML_URL" > ${CATALINA_HOME}/lib/org/apache/catalina/authenticator/mbeans-descriptors.xml
-
-AUTHENTICATOR_PROPERTIES_URL="https://raw.githubusercontent.com/eea/aqr-public/master/ServerConfiguration/ecas/tomcat/org/apache/catalina/startup/Authenticators.properties"
-mkdir -p ${CATALINA_HOME}/lib/org/apache/catalina/startup
-curl -L "${AUTHENTICATOR_PROPERTIES_URL}" > ${CATALINA_HOME}/lib/org/apache/catalina/startup/Authenticators.properties
-
-ECAS_TOMCAT_JAR_URL="https://github.com/eea/aqr-public/raw/master/ServerConfiguration/ecas/tomcat/ecas-tomcat-5.5-3.6.3.jar"
-LOG4J_JAR_URL="https://github.com/eea/aqr-public/raw/master/ServerConfiguration/ecas/tomcat/log4j-1.2.17.jar"
-#
-# TODO(ezyk) Move all these jar dependencies to maven if possible.
-# TODO(ezyk) Move Logging configuration. Logging configuration should be done when we build the war
-#
-curl -L "${ECAS_TOMCAT_JAR_URL}" > ${CATALINA_HOME}/lib/ecas-tomcat-5.5-3.6.3.jar
-curl -L "${LOG4J_JAR_URL}" > ${CATALINA_HOME}/lib/log4j-1.2.17.jar
-
-# Handle logging
-LOGGING4J_PROPERTIES_URL="https://github.com/eea/aqr-public/blob/master/ServerConfiguration/log4j/log4j.properties"
-curl -L "$LOGGING4J_PROPERTIES_URL" > ${CATALINA_HOME}/lib/log4j.properties
-
-TOMCAT_JULI_ADAPTERS_JAR_URL="https://github.com/eea/aqr-public/raw/master/ServerConfiguration/log4j/tomcat-juli-adapters.jar"
-curl -L "${TOMCAT_JULI_ADAPTERS_JAR_URL}" > ${CATALINA_HOME}/lib/tomcat-juli-adapters.jar
-
-TOMCAT_JULI_JAR_URL="https://github.com/eea/aqr-public/raw/master/ServerConfiguration/log4j/tomcat-juli.jar"
-curl -L "${TOMCAT_JULI_JAR_URL}" > ${CATALINA_HOME}/lib/tomcat-juli.jar
-
-# Remove logging.properties file from tomcat
-rm ${CATALINA_HOME}/conf/logging.properties
-
-#
-# Configure tomcat authentication
-#
-sed -i 's#<Realm className="org.apache.catalina.realm.UserDatabaseRealm"#<Realm className="org.apache.catalina.realm.UserDatabaseRealm" allRolesMode="authOnly"#' $CATALINA_HOME/conf/server.xml
-
-
-
-# URL location of the certificates
-EUROCOM_CERT_URL="https://raw.githubusercontent.com/eea/aqr-public/master/ServerConfiguration/ecas/certs/CommisSign.cer"
-COMMISSIGN_CERT_URL="https://raw.githubusercontent.com/eea/aqr-public/master/ServerConfiguration/ecas/certs/EuropeanCommission.cer"
-GLOBALSIGN_CERT_URL="https://raw.githubusercontent.com/eea/aqr-public/master/ServerConfiguration/ecas/certs/GlobalSign.cer"
-
-# Expected SHA1 values for EUROPEANCOMMISSION, COMMISSIGN and GLOBALSIGN certificates
-EUROCOM_CERT_SHA1_DEFAULT="70a7c70604a20c0fedc704351637efb9ff298b4f"
-COMMISSIGN_CERT_SHA1_DEFAULT="b7e343a36e8bfbe5154250c1987f2efc6c396abb"
-GLOBALSIGN_CERT_SHA1_DEFAULT="53a2b04bca6bd645e6398a8ec40dd2bf77c3a290"
-
-
-# If cacert password hasn't changed, set to default value
-if [ -z "${KEYSTORE_PASS}" ]; then
-	KEYSTORE_PASS="changeit"
-fi
-
-# Download certificates
-mkdir -p /tmp/certs
-curl -L "${EUROCOM_CERT_URL}" > /tmp/certs/CommisSign.cer 
-curl -L "${COMMISSIGN_CERT_URL}" >/tmp/certs/EuropeanCommission.cer
-curl -L "${GLOBALSIGN_CERT_URL}" >/tmp/certs/GlobalSign.cer
-
-# Get the SHA1 from the certificates that we downloaded.
-EUROCOM_CERT_SHA1_FROM_FILE=$(sha1sum /tmp/certs/EuropeanCommission.cer | awk '{print $1}')
-COMMISSIGN_CERT_SHA1_FROM_FILE=$(sha1sum /tmp/certs/CommisSign.cer | awk '{print $1}')
-GLOBALSIGN_CERT_SHA1_FROM_FILE=$(sha1sum /tmp/certs/GlobalSign.cer | awk '{print $1}')
-
-# Check if the certificates are corrupted or invalid.
-if [ "${EUROCOM_CERT_SHA1_FROM_FILE}" = "${EUROCOM_CERT_SHA1_DEFAULT}" ] && [ "${COMMISSIGN_CERT_SHA1_FROM_FILE}" = "${COMMISSIGN_CERT_SHA1_DEFAULT}" ] && [ "${GLOBALSIGN_CERT_SHA1_FROM_FILE}" = "${GLOBALSIGN_CERT_SHA1_DEFAULT}" ]; then
-	printf "Invalid sha1sum for certificates trying to import.\n"
-	exit 1
-fi
-
-# Import certificates
-keytool -import -noprompt -v -keystore cacerts -storepass ${KEYSTORE_PASS} -alias EuropeanCommission -file /tmp/certs/EuropeanCommission.cer
-keytool -import -noprompt -v -keystore cacerts -storepass ${KEYSTORE_PASS} -alias CommisSign -file /tmp/certs/CommisSign.cer
-keytool -import -noprompt -v -keystore cacerts -storepass ${KEYSTORE_PASS} -alias GlobalSign -file /tmp/certs/GlobalSign.cer
-
-
-# Delete the old Root directory
+# Delete default Root directory if present
 if [ -d "${CATALINA_HOME}/webapps/ROOT" ]; then
-	rm -rf ${CATALINA_HOME}/webapps/ROOT
+    rm -rf ${CATALINA_HOME}/webapps/ROOT
 fi
 
-if [ -n "$CURL_URL" ]; then
-	if [ -n "$CURL_USER"] && [ -n "$CURL_PASSWORD" ]; then
-		curl -o $WAR_FILEPATH -u "${CURL_USER}:${CURL_PASSWORD}" "$CURL_URL"
-	else
-		curl -o $WAR_FILEPATH "$CURL_URL"
-	fi
-	
-fi
-
-# Check if war file exists
+# Ensure the war file is in place
 if [ ! -f "$WAR_FILEPATH" ]; then
-	printf "Couldn't find the war file in %s\n" "$WAR_FILEPATH"
-	ls -la $WAR_FILEPATH
-	exit 1
+    printf "Critical Error: Found no application war file at %s\n" "$WAR_FILEPATH"
+    exit 1
 fi
 
-cp ${WAR_FILEPATH} $CATALINA_HOME/webapps/ROOT.war
+cp "${WAR_FILEPATH}" "${CATALINA_HOME}/webapps/ROOT.war"
 
-# Run tomcat server
-${CATALINA_HOME}/bin/catalina.sh run
-
+# Execute tomcat
+exec ${CATALINA_HOME}/bin/catalina.sh run
